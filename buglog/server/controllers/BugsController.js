19c0e7548328a1f6d0af67da04219d78b5caa6ca
cjgreenie <seejarom@gmail.com>
2021-08-13 23:36:05 -0600

    bug controller/service setup

--- buglog/server/controllers/BugsController.js
+++ buglog/server/controllers/BugsController.js
@@ -8,14 +8,83 @@ export class BugsController extends BaseController {
     this.router
     // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
       .use(Auth0Provider.getAuthorizedUserInfo)
+      .get('', this.getAll)
+      .get('/:id', this.getById)
       .post('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.destroy)
   }
 
+  /**
+   * Get bugs
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   * @param {import("express").NextFunction} next
+   */
+  async getAll(req, res, next) {
+    try {
+      const bug = await bugsService.getAll(req.query)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Get bug by id
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   * @param {import("express").NextFunction} next
+   */
+  async getById(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const bug = await bugsService.getById(req.params.id)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Create Bug
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   * @param {import("express").NextFunction} next
+   */
   async create(req, res, next) {
     try {
       // Do not trust the client
       req.body.creatorId = req.userInfo.id
       const bug = await bugsService.create(req.body)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      delete req.body.Closed
+      req.body.id = req.params.id
+      const bug = await bugsService.edit(req.body)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Delete bug
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   * @param {import("express").NextFunction} next
+   */
+  async destroy(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const bug = await bugsService.destroy(req.params.id, req.userInfo.id)
       res.send(bug)
     } catch (error) {
       next(error)
--- buglog/server/services/BugsService.js
+++ buglog/server/services/BugsService.js
@@ -1,9 +1,40 @@
 import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
 
 class BugsService {
+  async getAll(query = {}) {
+    const bugs = await dbContext.Bugs.find(query).populate('creator', 'name picture')
+    return bugs
+  }
+
+  async getById(id) {
+    const bug = await dbContext.Bugs.findById(id).populate('creator', 'name picture')
+    if (!bug) {
+      throw new BadRequest('Invalid Id')
+    }
+    return bug
+  }
+
   async create(body) {
     const bug = await dbContext.Bugs.create(body)
     return await dbContext.Bugs.findById(bug._id).populate('creator', 'name picture')
+  }
+
+  async edit(body) {
+    const bug = await dbContext.Bugs.findByIdAndUpdate(body.id, body, { new: true, runValidators: true })
+    // findOneAndUpdate
+    if (!bug) {
+      throw new BadRequest('Invalid Id')
+    }
+    return bug
+  }
+
+  async destroy(id, userId) {
+    const bug = await dbContext.Bugs.findOneAndUpdate({ _id: id, creatorId: userId }, { closed: true })
+    if (!bug) {
+      throw new BadRequest('Invalid Id')
+    }
+    return bug
   }
 }
 
